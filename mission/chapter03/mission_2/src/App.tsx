779dedca9b1733067bc2bcc6c271847479a3391c
채은 <thscodms04@naver.com>
2026-04-05 16:13:20 +0900

    mission: chapter03 mission_2 피어 리뷰 후 수정

--- mission/chapter03/mission_2/src/App.tsx
+++ mission/chapter03/mission_2/src/App.tsx
@@ -1,30 +1,21 @@
 import './App.css';
 import HomePage from './pages/HomePage';
 import MoviePage from './pages/MoviePage';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFoundPage from './pages/NotFoundPage';
 import MovieDetailPage from './pages/MovieDetailPage';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: 'movies/:category',
-        element: <MoviePage />,
-      },
-      //영화 상세페이지로
-      {
-        path: 'movies/:movieId',
-        element: <MovieDetailPage />
-      }
-    ]
-  }
-])
 function App() {
-  return<RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} errorElement={<NotFoundPage />}>
+          <Route path="movies/:category" element={<MoviePage />} />
+          <Route path="movies/:movieId" element={<MovieDetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- mission/chapter03/mission_2/src/components/MovieCard.tsx
+++ mission/chapter03/mission_2/src/components/MovieCard.tsx
@@ -10,19 +10,13 @@ interface MovieCardProps {
 }
 
 export default function MovieCard({ movie }: MovieCardProps) {
-  //hover 됐는지 아닌지에 대한 useState
-  const [isHovered, setIsHoverd] = useState(false);
   const navigate = useNavigate();
 
-  //<img /> 카드의 이미지 불러오기 + alt로 이미지 안 나올 때 에러처리
-  //isHoverd로 마우스 갖다댈 때 처리
   return (
     <div
       onClick={() => navigate(`/movies/${movie.id}`)}
-      className="relative rounded-xl shadow-lg overflow-hidden cursor-pointer w-44
+      className="group relative rounded-xl shadow-lg overflow-hidden cursor-pointer w-44
       transition-transform duration-500 hover:scale-105"
-      onMouseEnter={() => setIsHoverd(true)}
-      onMouseLeave={() => setIsHoverd(false)}
     >
       <img
         src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
@@ -30,20 +24,19 @@ export default function MovieCard({ movie }: MovieCardProps) {
         className=""
       />
 
-      {isHovered && (
-        <div
-          className="absolute inset-0 bg-gradient-to-t from-black/50
+      <div
+        className="absolute inset-0 bg-gradient-to-t from-black/50
         to-transparent backdrop-blur-md flex flex-col justify-center
-        items-center text-white p-4"
-        >
-          <h2 className="text-lg font-bold text-center leading-snug">
-            {movie.title}
-          </h2>
-          <p className="text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">
-            {movie.overview}
-          </p>
-        </div>
-      )}
+        items-center text-white p-4
+        opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+      >
+        <h2 className="text-lg font-bold text-center leading-snug">
+          {movie.title}
+        </h2>
+        <p className="text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">
+          {movie.overview}
+        </p>
+      </div>
     </div>
   );
 }
