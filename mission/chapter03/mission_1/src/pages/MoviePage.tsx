24a256e3ab89ff9a4c01f7167de6d7b3a1852ec5
채은 <thscodms04@naver.com>
2026-04-05 16:33:56 +0900

    mission: chapter03 mission_1 피어리뷰 후 수정

--- mission/chapter03/mission_1/src/pages/MoviePage.tsx
+++ mission/chapter03/mission_1/src/pages/MoviePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import type { Movie, MovieResponse } from '../types/movie';
+import type { Movie } from '../types/movie';
 import MovieCard from '../components/MovieCard';
+import { getPopularMovies } from '../apis/api';
 
 export default function MoviePage() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -9,15 +9,7 @@ export default function MoviePage() {
   //useEffect 이용하여 데이터 불러옴
   useEffect(() => {
     const fetchMovies = async () => {
-      const { data } = await axios.get<MovieResponse>(
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
-        {
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
-          },
-        },
-      );
-
+      const data = await getPopularMovies();
       setMovies(data.results);
     };
 
@@ -25,7 +17,7 @@ export default function MoviePage() {
   }, []);
 
   return (
-    <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6'>
+    <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
       {movies?.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
